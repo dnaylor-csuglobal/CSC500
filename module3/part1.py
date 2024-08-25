c38d4040a8e0f5892ff7f34bccc2ffb8ff23ed8f
dnaylor <darren.naylor@csuglobal.edu>
2024-08-25 09:37:42 -0500

    Final version of module 3

--- module3/part1.py
+++ module3/part1.py
@@ -1,17 +1,43 @@
-def read_float():
+"""
+Part 1:
+Write a program that calculates the total
+amount of a meal purchased at a restaurant.
+The program should ask the user to enter the
+charge for the food and then calculate the
+amounts with an 18 percent tip and 7 percent
+sales tax.
+Display each of these amounts and the total price.
+"""
+
+# constants
+SALES_TAX_PERCENT = 7
+SERVICE_CHARGE_PERCENT = 18
+
+
+# function that collects a non-zero float from standard input
+def read_non_negative_float():
     while True:
         try:
-            return float(input())
+            value = float(input())
+            if value < 0:
+                raise ValueError
+            return value
         except:
-            print("Input is not a valid amount. Please try again: ")
+            print("Input must be a non-negative value. Please try again: ")
 
 
-print("Please enter the charge for food: ")
-food_charge = round(read_float(), 2)
-print(f"Food Charge: ${food_charge:,.2f}")
-sales_tax = round(0.07 * food_charge, 2)
-print(f"Sales Tax: ${sales_tax:,.2f} (7%)")
-tip = round(0.18 * (food_charge + sales_tax), 2)
-print(f"Tip: ${tip:,.2f} (18%)")
-total_charge = food_charge + sales_tax + tip
-print(f"Total: ${total_charge:,.2f}")
+print("Please enter the dollar amount charge for food: ")
+food_charge = round(read_non_negative_float(), 2)  # round value to two decimal places
+print(f"Food Charge: ${food_charge:,.2f}")  # comma separated to 2 decimal places
+
+# calculate the sales tax
+sales_tax = round(SALES_TAX_PERCENT * food_charge / 100.00, 2)  # round value to two decimal places
+print(f"Sales Tax: ${sales_tax:,.2f} ({SALES_TAX_PERCENT}%)")  # comma separated to 2 decimal places
+
+# calculate the service charge
+service_charge = round(SERVICE_CHARGE_PERCENT * (food_charge + sales_tax) / 100.00, 2) # round value to two decimal places
+print(f"Gratuity: ${service_charge:,.2f} ({SERVICE_CHARGE_PERCENT}%)")  # comma separated to 2 decimal places
+
+# calculate the final amount
+total_charge = food_charge + sales_tax + service_charge
+print(f"Total: ${total_charge:,.2f}")  # comma separated to 2 decimal places
--- module3/part2.py
+++ module3/part2.py
@@ -1,3 +1,14 @@
+"""
+Part 2:
+Many people keep time using a 24-hour clock (11 is 11am and 23 is 11pm, 0 is midnight).
+If it is currently 13, and you set your alarm to go off in 50 hours, it will be 15 (3pm).
+Write a Python program to solve the general version of the above problem.
+Ask the user for the time now (in hours) and then ask for the number of hours to wait for the alarm.
+Your program should output what the time will be on a 24-hour clock when the alarm goes off.
+"""
+
+
+# read the hour between 0 and 24 inclusive from standard input with retry
 def read_hour():
     while True:
         try:
@@ -10,7 +21,8 @@ def read_hour():
             print("Input is not a valid hour of the day. Please enter a number between 0 and 23 inclusive.")
 
 
-def read_positive_int():
+# read a non-negative integer from standard input with retry
+def read_non_negative_int():
     while True:
         try:
             value = int(input())
@@ -19,14 +31,17 @@ def read_positive_int():
 
             return value
         except:
-            print("Input must be a positive integer. Please try again")
+            print("Input must be a non-negative integer. Please try again")
 
 
 print("Please enter the current hour of the day (0-23):")
 current_hour = read_hour()
 print(f"You entered {current_hour}")
+
 print("Please enter the number of hours to set the alarm:")
-delay_hours = read_positive_int()
+delay_hours = read_non_negative_int()
 print(f"You entered {delay_hours}")
-alarm_hour = (current_hour + delay_hours) % 24
-print(f"The alarm is set for hour {alarm_hour}")
+
+alarm_hour = (current_hour + delay_hours) % 24  # modulo operator
+alarm_days = (current_hour + delay_hours) // 24  # integer division
+print(f"The alarm is set for hour {alarm_hour} in {alarm_days} days.")
